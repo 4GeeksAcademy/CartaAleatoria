b97096db1cbe6de081e73e48b6207ae162bd2016
Alejandro <alejandrolujan1999@outlook.es>
2024-10-17 21:31:03 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/cards.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Declaramos dos arrays, uno con los palos y otro con los valores de cada carta
+  let palo = ["♥", "♦", "♣", "♠"];
+  let carta = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "🦹🏻"];
+  let color = "";
+
+  let cartaRandom = Math.floor(Math.random() * carta.length);
+  let paloRandom = Math.floor(Math.random() * palo.length);
+
+  if (palo[paloRandom] === "♦" || palo[paloRandom] === "♥") {
+    color = "red";
+  } else {
+    color = "black";
+  }
+
+  document.querySelector(".generarCarta").innerHTML = carta[cartaRandom];
+  document.querySelector(".generarPaloTop").innerHTML = palo[paloRandom];
+  document.querySelector(".generarPaloBottom").innerHTML = palo[paloRandom];
+
+  document.querySelector(".generarCarta").style.color = color;
+  document.querySelector(".generarPaloTop").style.color = color;
+  document.querySelector(".generarPaloBottom").style.color = color;
 };
